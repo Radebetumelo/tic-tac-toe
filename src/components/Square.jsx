605372fafc27c3bf585365c931ceca094e1bf803
Tumelo Radebe <radebetumelo8@gmail.com>
2025-05-21 12:17:00 +0200

    This is a second commit with these changes: working tic tac toegame, update on the ui (how the game looks), and i added the buttons for going back to history

---
+++ src/components/Square.jsx
@@ -0,0 +1,29 @@
+
+import img1 from "../images/O.png"
+import img2 from "../images/game_X.png"
+
+
+
+
+function Square( {value, onSquareClick} ) {
+
+    let content;
+    if (value === 'X'){
+        content = <img src={img2} alt="X" wdtih={80} height={80}/>;
+    } else if( value === 'O'){
+        content = <img src={img1} alt="O" wdtih={80} height={80}/>;
+    } else    {
+        content = null;
+    }
+
+    return (
+        <button 
+            className="square"
+            onClick={onSquareClick}
+        >
+            {content}
+        </button>
+    );
+}
+
+export default Square
